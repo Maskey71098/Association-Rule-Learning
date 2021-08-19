b55340c0108a352616b9d26b83f95bc20d2883c7
Maskey71098 <abhishekmaskey71098@gmail.com>
2021-08-19 15:33:40 +0545

    Final commit

--- Apriori.py
+++ Apriori.py
@@ -22,8 +22,10 @@ def inspect(results):
     lhs         = [tuple(result[2][0][0])[0] for result in results]
     rhs         = [tuple(result[2][0][1])[0] for result in results]
     supports    = [result[1] for result in results]
-    return list(zip(lhs, rhs, supports))
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
+    confidences = [result[2][0][2] for result in results]
+    lifts       = [result[2][0][3] for result in results]
+    return list(zip(lhs, rhs, supports, confidences, lifts))
+resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Left Hand Side', 'Right Hand Side', 'Support', 'Confidence', 'Lift'])
 
 #DISPLAYING THE RESULTS SORTED IN DESCENDING LIST
-print(resultsinDataFrame.nlargest(n = 10, columns = 'Support'))
+print(resultsinDataFrame.nlargest(n = 10, columns = 'Lift'))
